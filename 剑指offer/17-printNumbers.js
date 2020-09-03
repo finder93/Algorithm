8bc8db907aa9abaf44dd03f193d674ef17af455c
Johnny <cjx_whu@163.com>
2020-09-03 14:07:26 +0800

    update

--- 剑指offer/17-printNumbers.js
+++ 剑指offer/17-printNumbers.js
@@ -15,7 +15,6 @@ var printNumbers = function (n) {
 
 // 本质还是使用快速幂求出10^n次方
 // 快速幂：将指数转换成二进制形式，例如a^(13)=a^(2^0+2^2+2^3)=a(2^0)a(2^2)a(2^3)
-
 var printNumbers = function (n) {
     let base = 10,
         sum = 1,
@@ -39,33 +38,34 @@ var printNumbers = function (n) {
 };
 
 // 正确表示大数, 使用字符串表示
-var res;
-var nine = 0,
-    count = 0,
-    start,
-    _n;
-let num = [],
-    loop = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var printNumbers = function (n) {
-    res = [];
-    num = "";
-    start = n - 1;
-    dfs(0);
-    return res;
-};
-var dfs = function (x) {
-    if (x == n) {
-        let s = String.valueOf(num).substring(start);
-        if (s !== "0") res[count++] = Number.parseInt(s);
-        if (n - start == nine) start--;
-        return;
-    }
-    for (let i in loop) {
-        if (i == "9") nine++;
-        num[x] = i;
-        dfs(x + 1);
-    }
-    nine--;
+    if (n < 0) return;
+
+    let result = [];
+    let num = "";
+    /**
+     * 递归求全排列
+     * @param {string} num 用于拼接数字的字符串
+     * @param {number} length 最大位数
+     * @param {number} index  当前位数，从0开始
+     */
+    let printNumbersCore = function (num, length, index) {
+        // 判断有上一次递归传入的当前位数，若等于最大位数，则存入结果数组
+        if (index === length) {
+            result.push(num);
+            return;
+        }
+        for (let i = 0; i < 10; i++) {
+            let temp = num;
+            if (i !== 0 || num.length !== 0) num += i; // 从最高位开始，每确定一位，传入下一次递归，计算下一位
+            // 遇到以0开头的，不添加到num中
+            printNumbersCore(num, length, index + 1);
+            num = temp;
+        }
+    };
+    printNumbersCore(num, n, 0);
+
+    return result.slice(1); // 取掉数组第一个元素 0
 };
 
 console.log(printNumbers(3));
